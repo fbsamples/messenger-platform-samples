11981780f7fee3d5ed6176f2a229d58f313a2667
peter78 <peter78@gmail.com>
2016-07-08 16:45:00 -0700

    Forgot to remove old instance of starting server

--- node/app.js
+++ node/app.js
@@ -804,13 +804,6 @@ function callSendAPI(messageData) {
   });  
 }
 
-// Start server
-// Webhooks must be available via SSL with a certificate signed by a valid 
-// certificate authority.
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 // Start the server
 // Webhooks must be available via SSL with a certificate signed by a valid 
 // certificate authority.
