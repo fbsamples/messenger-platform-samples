b67bef5371c8fe6f1dd614dc662af242a64d47a0
peter78 <peter78@gmail.com>
2016-07-08 17:09:01 -0700

    Fix error output

--- app.js
+++ app.js
@@ -800,10 +800,7 @@ function callSendAPI(messageData) {
         recipientId);
       }
     } else {
-      var errorMessage = response.error.message;
-      var errorCode = response.error.code;
-      console.error("Unable to send message. Error %d: %s", 
-        errorCode, errorMessage);
+      console.error(response.error);
     }
   });  
 }
