76789e30050bee7306e94ec2d42063d324d35cf4
zolbayars <zolube@gmail.com>
2016-08-19 12:11:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-/*
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-/* jshint node: true, devel: true */
 'use strict';
 
 const 
