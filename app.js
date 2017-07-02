e3e1cb826550db984feb093bf489aaf50eed86ab
Simone Mariotti <simone@mariotti.me>
2017-07-02 19:04:22 +0200

    First call to AWS

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({verify: verifyRequestSignature}));
 app.use(express.static('public'));
-const rekognition = AWS.rekognition({
+const rekognition = new AWS.Rekognition({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
