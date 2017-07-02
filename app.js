a0386cb9ce02497477048ee2935c6e84e054928f
Simone Mariotti <simone@mariotti.me>
2017-07-02 19:14:41 +0200

    Modified URL

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static('public'));
 const rekognition = new AWS.Rekognition({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'eu-west-1',
 });
 
 /*
