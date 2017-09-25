06d0a3033d2145dd8c9b1fc8ed1a3148353e298d
Alex Muramoto <amuramoto@apache.org>
2017-09-25 16:37:00 -0700

    cleans up the code a bit

--- quick-start/app.js
+++ quick-start/app.js
@@ -41,18 +41,18 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = '<YOUR_VERIFY_TOKEN>';
+  let verify_token = '<YOUR_VERIFY_TOKEN>';
     
   // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  let mode = req.query['hub.mode'],
+      token = req.query['hub.verify_token'],
+      challenge = req.query['hub.challenge'];
     
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
   
     // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    if (mode === 'subscribe' && token === verify_token) {
       
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
