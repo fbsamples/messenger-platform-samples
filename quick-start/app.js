54c761cad76c0716292ad9398fed5c138e877791
Alex Muramoto <amuramoto@apache.org>
2017-09-25 16:27:19 -0700

    minor change to var names

--- quick-start/app.js
+++ quick-start/app.js
@@ -3,8 +3,8 @@
 // Imports dependencies and set up http server
 const 
   express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  body_parser = require('body-parser'),
+  app = express().use(body_parser.json()); // creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -22,8 +22,8 @@ app.post('/webhook', (req, res) => {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
-      let webhookEvent = entry.messaging[0];
-      console.log(webhookEvent);
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
       
     });
 
@@ -41,7 +41,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  let VERIFY_TOKEN = '<YOUR_VERIFY_TOKEN>';
     
   // Parse the query params
   let mode = req.query['hub.mode'];
