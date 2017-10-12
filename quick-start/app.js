dbffe2b0e7f0335722dff7960f86b691e9f50407
Alex Muramoto <amuramoto@apache.org>
2017-10-12 11:36:29 -0700

    harder equality check

--- quick-start/app.js
+++ quick-start/app.js
@@ -145,9 +145,9 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
 
   // Set the response based on the postback payload
-  if (payload == 'yes') {
+  if (payload === 'yes') {
     response = { "text": "Thanks!" }
-  } else if (payload == 'no') {
+  } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }
   // Send the message to acknowledge the postback
