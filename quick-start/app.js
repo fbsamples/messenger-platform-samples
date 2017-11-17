330ea4ba392608298c149cd8b246e09dad841293
Alex Muramoto <amuramoto@users.noreply.github.com>
2017-11-17 10:18:22 -0800

    Added copyright notice

--- quick-start/app.js
+++ quick-start/app.js
@@ -1,4 +1,10 @@
-/*
+
+/**
+ * Copyright 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
  * Messenger Platform Quick Start Tutorial
  *
  * This is the completed code for the Messenger Platform quick start tutorial
