7ebf2fca0ff63dc195ab8dd7e81e96906365e282
Alex Muramoto <amuramoto@apache.org>
2017-12-18 12:52:13 -0800

    adds copyright and use strict to all files

--- handover_protocol/utils/api.js
+++ handover_protocol/utils/api.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
+'use strict';
 const request = require('request');
 
 const call = (endPoint, messagePayload, callback) => {
--- handover_protocol/utils/handover-protocol.js
+++ handover_protocol/utils/handover-protocol.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
  */
+'use strict';
 const api = require('./api');
 
 const passThreadControl = (userPsid, targetAppId, metadata) => {
