a9a6da2451912f0e5d55c4e00dcd05b4b1fc2f28
Alex Muramoto <amuramoto@apache.org>
2017-12-18 12:50:54 -0800

    Simplify code, and standardize on using api.call

---
+++ handover_protocol/utils/quick-reply.js
@@ -0,0 +1,23 @@
+'use strict';
+const api = require('./api');
+
+function sendQuickReply(psid, message) {
+  let payload = {};
+
+  payload.recipient = {
+    id: psid
+  }
+  
+  payload.message = {
+    text: message,
+    quick_replies: [{
+        content_type: 'text',
+        title: 'handover',
+        payload: 'handover'
+    }]    
+  }
+
+  api.call('/me/messages', payload, () => {});
+}
+
+module.exports = SendAPIRequest;
