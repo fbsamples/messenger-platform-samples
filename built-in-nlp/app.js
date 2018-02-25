01f6fd7e08e6efacf05059ceb0972c86ed3b7d49
Chris Ward <chriswhward@gmail.com>
2018-02-25 10:16:43 +0100

    Furthe clean code

--- built-in-nlp/app.js
+++ built-in-nlp/app.js
@@ -20,8 +20,7 @@ const
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
-app.set('view engine', 'ejs');
-app.use(bodyParser.json({verify: verifyRequestSignature}));
+app.use(bodyParser.json());
 
 /*
  * Be sure to setup your config values before running this code. You can
@@ -89,18 +88,10 @@ app.post('/webhook', function (req, res) {
         data.entry.forEach(function (pageEntry) {
             // Iterate over each messaging event
             pageEntry.messaging.forEach(function (messagingEvent) {
-                if (messagingEvent.optin) {
-                    receivedAuthentication(messagingEvent);
-                } else if (messagingEvent.message) {
+                if (messagingEvent.message) {
                     receivedMessage(messagingEvent);
-                } else if (messagingEvent.delivery) {
-                    receivedDeliveryConfirmation(messagingEvent);
                 } else if (messagingEvent.postback) {
                     receivedPostback(messagingEvent);
-                } else if (messagingEvent.read) {
-                    receivedMessageRead(messagingEvent);
-                } else if (messagingEvent.account_linking) {
-                    receivedAccountLink(messagingEvent);
                 } else {
                     console.log("Webhook received unknown messagingEvent: ", messagingEvent);
                 }
@@ -114,87 +105,6 @@ app.post('/webhook', function (req, res) {
         res.sendStatus(200);
     }
 });
-
-/*
- * This path is used for account linking. The account linking call-to-action
- * (sendAccountLinking) is pointed to this URL.
- *
- */
-app.get('/authorize', function (req, res) {
-    var accountLinkingToken = req.query.account_linking_token;
-    var redirectURI = req.query.redirect_uri;
-
-    // Authorization Code should be generated per user by the developer. This will
-    // be passed to the Account Linking callback.
-    var authCode = "1234567890";
-
-    // Redirect users to this URI on successful login
-    var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
-
-    res.render('authorize', {
-        accountLinkingToken: accountLinkingToken,
-        redirectURI: redirectURI,
-        redirectURISuccess: redirectURISuccess
-    });
-});
-
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-function verifyRequestSignature(req, res, buf) {
-    var signature = req.headers["x-hub-signature"];
-
-    if (!signature) {
-        // For testing, let's log an error. In production, you should throw an
-        // error.
-        console.error("Couldn't validate the signature.");
-    } else {
-        var elements = signature.split('=');
-        var signatureHash = elements[1];
-
-        var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-            .update(buf)
-            .digest('hex');
-
-        if (signatureHash != expectedHash) {
-            throw new Error("Couldn't validate the request signature.");
-        }
-    }
-}
-
-/*
- * Authorization Event
- *
- * The value for 'optin.ref' is defined in the entry point. For the "Send to
- * Messenger" plugin, it is the 'data-ref' field. Read more at
- * https://developers.facebook.com/docs/messenger-platform/webhook-reference/authentication
- *
- */
-function receivedAuthentication(event) {
-    var senderID = event.sender.id;
-    var recipientID = event.recipient.id;
-    var timeOfAuth = event.timestamp;
-
-    // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
-    // The developer can set this to an arbitrary value to associate the
-    // authentication callback with the 'Send to Messenger' click event. This is
-    // a way to do account linking when the user clicks the 'Send to Messenger'
-    // plugin.
-    var passThroughParam = event.optin.ref;
-
-    console.log("Received authentication for user %d and page %d with pass " +
-        "through param '%s' at %d", senderID, recipientID, passThroughParam,
-        timeOfAuth);
-
-    // When an authentication is received, we'll send a message back to the sender
-    // to let them know it was successful.
-    sendTextMessage(senderID, "Authentication successful");
-}
 
 /*
  * Message Event
@@ -243,7 +153,7 @@ function receivedMessage(event) {
             if (email && email.confidence > 0.8) {
                 if (when && when.confidence > 0.8) {
                     // TODO: Perform action
-                    sendTextMessage(senderID, `Great! I will transfer ${money.unit}${money.value} to ${email.value} ${formattedWhen} `);
+                    sendTextMessage(senderID, `Great! I will transfer ${money.unit}${money.value} to ${email.value} ${formattedWhen}`);
                 } else {
                     sendTextMessage(senderID, "Sorry, I don't understand when to make the transfer.");
                 }
@@ -262,28 +172,6 @@ function searchNLP(nlp, name) {
     } else {
         // TODO: Error handle
     }
-}
-
-/*
- * Delivery Confirmation Event
- *
- * This event is sent to confirm the delivery of a message. Read more about
- * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
- *
- */
-function receivedDeliveryConfirmation(event) {
-    var delivery = event.delivery;
-    var messageIDs = delivery.mids;
-    var watermark = delivery.watermark;
-
-    if (messageIDs) {
-        messageIDs.forEach(function (messageID) {
-            console.log("Received delivery confirmation for message ID: %s",
-                messageID);
-        });
-    }
-
-    console.log("All message before %d were delivered.", watermark);
 }
 
 
@@ -309,39 +197,6 @@ function receivedPostback(event) {
     // When a postback is called, we'll send a message back to the sender to
     // let them know it was successful
     sendTextMessage(senderID, "Postback called");
-}
-
-/*
- * Message Read Event
- *
- * This event is called when a previously-sent message has been read.
- * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
- *
- */
-function receivedMessageRead(event) {
-    // All messages before watermark (a timestamp) or sequence have been seen.
-    var watermark = event.read.watermark;
-    var sequenceNumber = event.read.seq;
-
-    console.log("Received message read event for watermark %d and sequence " +
-        "number %d", watermark, sequenceNumber);
-}
-
-/*
- * Account Link Event
- *
- * This event is called when the Link Account or UnLink Account action has been
- * tapped.
- * https://developers.facebook.com/docs/messenger-platform/webhook-reference/account-linking
- *
- */
-function receivedAccountLink(event) {
-    var senderID = event.sender.id;
-    var status = event.account_linking.status;
-    var authCode = event.account_linking.authorization_code;
-
-    console.log("Received account link event with for user %d with status %s " +
-        "and auth code %s ", senderID, status, authCode);
 }
 
 /*
