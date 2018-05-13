d6ba3e234177d0bad7e6ea02b59f090915ee6376
Chris Ward <chrisward@fb.com>
2018-05-13 12:53:16 +0200

    Creat ecommerce bot start

---
+++ ecommerce/app.js
@@ -0,0 +1,101 @@
+'use strict';
+const PAGE_ACCESS_TOKEN = "";
+const SERVER_URL = "";
+// Imports dependencies and set up http server
+const
+    request = require('request'),
+    express = require('express'),
+    body_parser = require('body-parser'),
+    app = express().use(body_parser.json()); // creates express http server
+app.use(body_parser.urlencoded({extended: false}));
+
+// Stripe details
+const keyPublishable = "";
+const keySecret = "";
+const stripe = require("stripe")(keySecret);
+
+app.set("view engine", "pug");
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+
+let paymentsEnabled;
+
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {
+
+    // Parse the request body from the POST
+    let body = req.body;
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+
+        body.entry.forEach(function (entry) {
+
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+
+// Add user profile request
+
+        });
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+});
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+
+    /** UPDATE YOUR VERIFY TOKEN **/
+    const VERIFY_TOKEN = "";
+
+    // Parse params from the webhook verification request
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Check if a token and mode were sent
+    if (mode && token) {
+
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+    console.log(request_body);
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {"access_token": PAGE_ACCESS_TOKEN},
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!' + JSON.stringify(res));
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
