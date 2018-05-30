38189baae8650d84902a35d31e115fede5b9e4f2
Chris Ward <chrisward@fb.com>
2018-05-30 14:04:02 +0300

    Remove IDs

--- customer-chat/app.js
+++ customer-chat/app.js
@@ -137,7 +137,7 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": {"access_token": "TOKEN"},
+        "qs": {"access_token": ""},
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
